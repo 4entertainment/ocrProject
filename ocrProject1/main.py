db56a8b3be09eeaf39a3a117b4bb7719629fbd1d
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-09-04 16:11:01 +0300

    Add files via upload

--- ocrProject1/main.py
+++ ocrProject1/main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 from PIL import Image
 import pytesseract
 import fitz  # PyMuPDF
+import time
+
 
 app = Flask(__name__)
 
@@ -12,8 +14,8 @@ def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            text, filename = process_pdf(uploaded_file)  # PDF'i işleyip metne çevir ve dosya adını al
-            return render_template('index.html', text=text, filename=filename)  # HTML şablonuna dosya adını da gönder
+            text, filename, elapsed_time = process_pdf(uploaded_file)  # PDF'i işleyip metne çevir ve dosya adını al
+            return render_template('index.html', text=text, filename=filename, elapsed_time=elapsed_time)  # HTML şablonuna dosya adını da gönder
     return render_template('index.html', text=None)
 
 # PDF dosyasını işle ve metni çıkar
@@ -23,12 +25,17 @@ def process_pdf(pdf_file):
     pdf_document = fitz.open(stream=pdf_data, filetype='pdf')
     text = ""
 
+    start_time = time.time() # Record the start time
+
     for page_num in range(pdf_document.page_count):
         page = pdf_document.load_page(page_num)
         page_text = extract_text_from_page(page)  # Sayfadaki metni çıkar
         text += page_text
 
-    return text, pdf_file.filename  # Metni ve dosya adını döndür
+    end_time = time.time() # Record the end time
+    elapsed_time = end_time - start_time
+
+    return text, pdf_file.filename, elapsed_time  # Metni ve dosya adını döndür
 
 # Sayfadaki metni çıkarma
 def extract_text_from_page(page):
